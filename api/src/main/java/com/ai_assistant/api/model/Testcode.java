a8703f7e9952524fdb4b7e8b3415d6af376a34a4
HuanfuLi <huanfuli4408@gmail.com>
2025-04-20 16:43:24 -0400

    Added a test case for local file extraction and code debug test

---
+++ api/src/main/java/com/ai_assistant/api/model/Testcode.java
@@ -0,0 +1,56 @@
+package com.ai_assistant.api.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Testcode {
+    public int quickSelect(List<Integer> numList, int k){
+        if (numList.size() == 1){
+            return numList.get(0);
+        }
+        List<Integer> big = new ArrayList<Integer>();
+        List<Integer> equal = new ArrayList<Integer>();
+        List<Integer> small = new ArrayList<Integer>();
+        int pivot = numList.get(0);
+
+        for (int num : numList){
+            if (num > pivot){
+                big.add(num);
+            }
+            else if (num == pivot){
+                equal.add(num);
+            }
+            else {
+                small.add(num);
+            }
+        }
+
+        if (k <= big.size()) {
+            return quickSelect(big, k);
+        } else if (k == big.size() + equal.size()) {
+            return pivot;
+        } else {
+            return quickSelect(small, k - big.size() - equal.size());
+        }
+    }
+    
+    
+    
+    
+    public int findKthLargest(int[] nums, int k) {
+        List<Integer> numList = new ArrayList<Integer>();
+        for (int num : nums){
+            numList.add(num);
+        }
+        return quickSelect(numList, k);
+    }
+
+    public static void main(String[] args) {
+        int[] input = {3,2,1,5,6,4};
+        int k = 2;
+        Testcode test = new Testcode();
+        int result = test.findKthLargest(input, k);
+        System.out.println(result);
+    }
+}
+
